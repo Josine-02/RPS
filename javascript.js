43fee17463d057b8cce289aa58c9656d30e1d083
Josine-02 <josine.deleeuwvanweenen@gmail.com>
2025-02-25 23:27:02 +0100

    add scores

--- javascript.js
+++ javascript.js
@@ -1,9 +1,6 @@
-
-
 // plan:
 // -create a function that returns rock / paper or scissors randomly for the computer
 // -create a function for human input 
-// -create a function that saves the human input
 // -create make sure that the two answers are compared
 // -create a message if you won 
 
@@ -32,40 +29,42 @@ getComputerChoice();
 let scoreComputer =0;
 let scoreHuman =0;
 
+console.log(typeof(humanChoice));
 
-function declareOutcome() {
+// let humanChoiceNew;
+//     string humanChoiceNew = humanChoice.toLowerCase();
+
+
+function playRound(humanChoice, computerChoice) {
+    
     if (humanChoice == computerChoice) {
         console.log("It's a tie!")
     } else if (humanChoice == "scissor" && computerChoice == "paper"){
-        console.log("You won!")
+        console.log("You won! Scissor beats paper")
         scoreHuman++;
     } else if (humanChoice == "scissor" && computerChoice == "rock") {
-        console.log(`You lost!`)
+        console.log(`You lost! Rock beats scissor`)
         scoreComputer++;
     } else if (humanChoice == "rock" && computerChoice == "scissor") {
-        console.log(`You won!`)
+        console.log(`You won! Rock beats scissor`)
         scoreHuman++;
     } else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log(`You lost!`)
+        console.log(`You lost! Paper beats rock`)
         scoreComputer++;
     } else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log(`You won!`)
+        console.log(`You won! Paper beats rock`)
         scoreHuman++;
     } else if (humanChoice == "paper" && computerChoice == "scissor") {
-        console.log(`You lost!`)
+        console.log(`You lost! Scissor beats paper`)
         scoreComputer++;
     } else {console.log(`You had a type error`)}
-}
 
-declareOutcome();
-
-function showScore(){
     console.log(`Score \n Computer: ${scoreComputer} \n You: ${scoreHuman}`
     )
 }
 
-showScore();
+playRound()
 
-function playRound(humanChoice, computerChoice){
-
+function playGame(){
+    
 }
